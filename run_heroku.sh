71c87bd170872de858c7d06cfe1d4de2b7bd9e6c
nbever <cracktarts@hotmail.com>
2018-08-14 22:53:23 -0600

    using install hooks

--- run_heroku.sh
+++ run_heroku.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
-pushd server
-npm install
-rm -rf ../heroku/node_modules
-mv node_modules ../heroku
-popd
-pushd heroku
+cd heroku
 MFG_MONGO_URI=mongodb://myfantasygolf:myfantasygolf1@ds221242.mlab.com:21242/myfantasygolf node app.js
